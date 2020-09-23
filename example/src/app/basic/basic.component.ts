bd1c49147524a121f85369c2759a6b130be93fc6
catzillaorz <catzillaorz@outlook.com>
2020-09-23 17:06:09 +0800

    画直线，并获取坐标

--- example/src/app/basic/basic.component.ts
+++ example/src/app/basic/basic.component.ts
@@ -1,37 +1,49 @@
-import {Component} from '@angular/core';
+import { Component } from '@angular/core';
+import { Feature, geom } from 'openlayers';
+import {toStringXY} from 'ol/coordinate';
+
+
 
 @Component({
-    selector: 'app-root',
-    templateUrl: './basic.component.html'
+  selector: 'app-root',
+  templateUrl: './basic.component.html'
 })
 export class BasicComponent {
   public zoom = 15;
   public opacity = 1.0;
   public width = 5;
+  selectLayer = 'Circle'
+  selectEdit = false
+
+  endCircle(feat: Feature) {
+    const polygon = feat.getGeometry() as geom.Polygon;
+    console.log(polygon.getCoordinates());
+
+  }
 
   _x_cordinate = 114.157153
   _y_cordinate = 30.489362
 
   increaseZoom() {
-    this.zoom  = Math.min(this.zoom + 1, 18);
+    this.zoom = Math.min(this.zoom + 1, 18);
     console.log('zoom: ', this.zoom);
   }
 
   decreaseZoom() {
-    this.zoom  = Math.max(this.zoom - 1, 1);
+    this.zoom = Math.max(this.zoom - 1, 1);
     console.log('zoom: ', this.zoom);
   }
 
   increaseOpacity() {
-    this.opacity  = Math.min(this.opacity + 0.1, 1);
+    this.opacity = Math.min(this.opacity + 0.1, 1);
     console.log('opacity: ', this.opacity);
   }
 
   decreaseOpacity() {
-    this.opacity  = Math.max(this.opacity - 0.1, 0);
+    this.opacity = Math.max(this.opacity - 0.1, 0);
     console.log('opacity: ', this.opacity);
   }
-  onDrawEnd(){
+  onDrawEnd() {
     console.log('onDrawEnd');
   }
 }
